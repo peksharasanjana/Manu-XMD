6cf5138326bcb2782eebf00e7beaa0066b82ff31
peksharasanjana <ddmaduwa567@gmail.com>
2025-03-11 00:07:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&UG0DgQCL#xKtJ1sCy5CFM9QKKUtBglbrmVLjfNfw1EV2bvDMLyd8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
